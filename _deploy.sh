71834b86aab59074b1e61a898836e5d32fc295d0
Yufree <yufree@live.cn>
2017-06-20 19:06:17 -0400

    update

--- _deploy.sh
+++ _deploy.sh
@@ -11,6 +11,6 @@ git config --global user.name "yufree"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
-git add --all *
+git add --all -- *
 git commit -m "Update the book" || true
 git push origin gh-pages
